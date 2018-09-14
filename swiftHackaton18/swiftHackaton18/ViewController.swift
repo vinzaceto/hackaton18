d50705b2cc0f05f41da762f7984341a85247edf9
vincenzo aceto <vincenzo.aceto-apple@unina.it>
2018-09-14 19:20:20 +0200

    first real commit

---
+++ swiftHackaton18/swiftHackaton18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftHackaton18
+//
+//  Created by Aceto Vincenzo on 14/09/18.
+//  Copyright © 2018 Aceto Vincenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
